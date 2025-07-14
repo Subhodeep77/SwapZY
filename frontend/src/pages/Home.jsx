1f3adb17f24b92c8340233f74f187c84ac2ca6a8
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2025-07-14 20:49:11 +0530

    Updated files of frontend.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,29 @@
-import HowItWorksSection from "../components/HowItWorksSection";
+import PageHelmet from "../components/PageHelmet";
+import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import FeatureSection from "../components/FeatureSection";
+import HowItWorksSection from "../components/HowItWorksSection";
 import ProductHighlights from "../components/ProductHighlights";
 import TestimonialSection from "../components/TestimonialSection";
 import FAQSection from "../components/FAQSection";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <PageHelmet
+        title="Home"
+        description="Explore SwapZY – your campus marketplace for buying, selling, and swapping products easily. Discover how it works and what makes it awesome."
+      />
+
+      <Header />
       <HeroSection />
       <FeatureSection />
-      <HowItWorksSection /> {/* ✅ Add it here */}
+      <HowItWorksSection />
       <ProductHighlights />
       <TestimonialSection />
-      <FAQSection/>
-      <Footer/>
-      {/* CTA or FAQ next */}
+      <FAQSection />
+      <Footer />
     </>
   );
 };
